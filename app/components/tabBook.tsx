8ce9374373bde360dab0ac851140535cd895897a
SIER101 <73215103+SIER101@users.noreply.github.com>
2025-03-30 04:34:58 +0700

    investv3

--- app/components/tabBook.tsx
+++ app/components/tabBook.tsx
@@ -3,8 +3,8 @@ import "./tabBook.css";
 
 export default function TabBook({ children }: { children: React.ReactNode }) {
   return (
-    <div className="tab-book w-full h-full">
-      <div className="flex flex-row">{children}</div>
+    <div className="tab-book w-full h-full min-h-[414px] flex flex-col">
+      <div className="flex flex-row h-full grow-1">{children}</div>
     </div>
   );
 }
--- app/investment/page.tsx
+++ app/investment/page.tsx
@@ -7,47 +7,100 @@ export default function InvestmentPage() {
   const [shares, setShares] = useState(10);
   const [selectedTab, setSelectedTab] = useState<Category>("Stock");
   const [selectedInvestment, setSelectedInvestment] = useState<number>(0);
+  const [isPop, setIsPop] = useState<boolean>(false);
 
-  // ─── ASSETS ───────────────────────────────────────────────────────────────
-  const assets: Record<Category, any[]> = {
+  const investments: Record<Category, any[]> = {
     Crypto: [
       {
         imageUrl: "graph/candleGraph.png",
         title: "SKBD Coin",
+        symbol: "SKBD",
         price: 30,
+      },
+      {
+        imageUrl: "graph/candleGraph.png",
+        title: "SGMA Coin",
+        symbol: "SGMA",
+        price: 67,
+      },
+      {
+        imageUrl: "graph/candleGraph.png",
+        title: "DIGE Coin",
+        symbol: "DIGE",
+        price: 120,
       },
     ],
     Stock: [
       {
         imageUrl: "graph/candleGraph.png",
         title: "NVIDI Corp (High Risk)",
+        symbol: "NVDI",
         price: 109,
+      },
+      {
+        imageUrl: "graph/candleGraph.png",
+        title: "J.K.Mogging (Moderate Risk)",
+        symbol: "JKM",
+        price: 130,
+      },
+      {
+        imageUrl: "graph/candleGraph.png",
+        title: "Nikola Inc (Low Risk)",
+        symbol: "NKLA",
+        price: 70,
+      },
+      {
+        imageUrl: "graph/candleGraph.png",
+        title: "SnP 50 (Mutual Funds)",
+        symbol: "SNP",
+        price: 200,
       },
     ],
     "Government Bond": [
       {
         imageUrl: "graph/candleGraph.png",
         title: "Yield to Maturity 5 Year",
-        price: 1,
+        symbol: "GOVT YTM 5",
+        price: 1000,
+      },
+      {
+        imageUrl: "graph/candleGraph.png",
+        title: "Yield to Maturity 10 Year",
+        symbol: "GOVT YTM 10",
+        price: 1000,
       },
     ],
     Lottery: [
       {
         imageUrl: "graph/candleGraph.png",
         title: "Lottery",
-        price: 3,
+        price: 5,
       },
     ],
   };
 
   return (
-    <div className="text-black p-4 flex flex-col items-center">
+    <div className="text-black p-3 flex flex-col items-center">
+      {isPop ? (
+        <div className="flex flex-col fixed left-[50%] top-[50%] z-50 translate-x-[-50%] translate-y-[-50%] shadow-lg w-4/5 rounded-2xl border-2 border-[#b6b885] bg-white px-4 py-4 items-center gap-2 text-base">
+          <h2 className="font-semibold text-center text-3xl">{selectedTab} Bought</h2>
+          You've bought {shares} of {investments[selectedTab][selectedInvestment].symbol}
+          <button
+            onClick={() => setIsPop(false)}
+            className="rounded-full border-2 border-black bg-[#b6b885] px-6 py-1.5"
+          >
+            Ok
+          </button>
+        </div>
+      ) : (
+        ""
+      )}
       <img
         src="investSign/investSign.png"
         alt="Invest Sign"
-        className="absolute top-8 w-[350px]"
+        className="relative -top-5 w-[350px]"
       />
-      <div className="relative overflow-clip bg-gray-900 text-white py-2 mt-11 w-[393px]">
+      <div className="relative overflow-clip bg-gray-900 text-white py-2 -mt-4 w-[393px]">
         <div className="ticker-container flex whitespace-nowrap w-max">
           <div className="ticker-item inline-flex items-center mx-4">
             <span className="font-bold text-green-400">APLE</span>
@@ -93,13 +146,13 @@ export default function InvestmentPage() {
         </div>
       </div>
 
-      <div className="bg-white p-2 rounded-lg mb-4 w-full mt-2 h-[200px] flex flex-col">
+      <div className="relative bg-white p-2 rounded-lg mb-4 w-full mt-2 h-[200px] flex flex-col">
         <p className="font-semibold absolute text-[#ffffff] mx-2 my-1">
-          NVDI $109
+          {investments[selectedTab][selectedInvestment].symbol}
         </p>
         <div className="bg-black rounded flex items-center justify-center h-full w-full">
           <img
-            src={assets[selectedTab][selectedInvestment].imageUrl}
+            src={investments[selectedTab][selectedInvestment].imageUrl}
             alt="Invest Sign"
             className="h-full w-full object-cover"
           />
@@ -107,7 +160,7 @@ export default function InvestmentPage() {
       </div>
       <TabBook>
         <div className="book p-4 bg-white rounded-l-xl w-full flex flex-col items-center">
-          <div className="mb-4">
+          <div className="mb-4 grow-1 flex flex-col">
             <div className="flex space-x-2 mb-2 items-center text-sm">
               Range
               <button className="bg-[#ffffff] ml-1 px-2 py-1 rounded-xl border-1">
@@ -124,51 +177,70 @@ export default function InvestmentPage() {
               </button>
             </div>
 
-            <div className="flex flex-col space-y-2">
-              <button className="bg-yellow-400 font-semibold px-2 py-1 rounded border-1">
-                NVIDI Corp (High Risk)
-              </button>
-              <button className="bg-[#ffffff] px-2 py-1 rounded border-1">
-                J.K.Mogging (Moderate Risk)
-              </button>
-              <button className="bg-[#ffffff] px-2 py-1 rounded border-1">
-                Nikola Inc (Low Risk)
-              </button>
-              <button className="bg-[#ffffff] px-2 py-1 rounded border-1">
-                SnP 50 (Mutual Funds)
-              </button>
+            <div className="flex flex-col space-y-2 grow-1 justify-center">
+              {Object.entries(investments[selectedTab]).map(
+                ([key, value], index) => (
+                  <button
+                    key={key}
+                    className={`${
+                      selectedInvestment == index
+                        ? `bg-yellow-400`
+                        : `bg-[#ffffff]`
+                    } font-semibold px-2 py-1 rounded border-1`}
+                    onClick={() => setSelectedInvestment(index)}
+                  >
+                    {value.title}
+                  </button>
+                )
+              )}
             </div>
           </div>
           <div className="mb-2 flex">
             <div className="flex flex-col space-x-2 w-3/5 mr-2">
               <div className="flex space-x-2 w-full mb-2 items-center">
                 <span className="text-skin min-w-[84px]">Stop Loss</span>
-                <div className="bg-[#ffffff] px-2 py-1 rounded border-1 text-skin w-full">
-                  $98
-                </div>
+                <input
+                  type="number"
+                  id="stopLoss"
+                  className="bg-[#ffffff] px-2 py-1 rounded border-1 text-skin w-full"
+                  placeholder="$"
+                />
               </div>
               <div className="flex space-x-2 w-full  items-center">
                 <span className="text-skin min-w-[84px]">Take Profit</span>
-                <div className="bg-[#ffffff] px-2 py-1 rounded border-1 text-skin w-full">
-                  $115
-                </div>
+                <input
+                  type="number"
+                  id="takeProfit"
+                  className="bg-[#ffffff] px-2 py-1 rounded border-1 text-skin w-full"
+                  placeholder="$"
+                />
               </div>
             </div>
 
             <div className="flex flex-col w-2/5 items-center justify-center">
               <span className="px-2 py-1 rounded border-1 text-skin mb-2">
-                Shares
+                Amount
               </span>
               <div className="w-full flex justify-center">
                 <button
                   className="bg-[#ffffff] px-2 rounded-2xl border-1 w-[35px]"
-                  onClick={() => setShares(Math.max(1, shares - 1))}
+                  onClick={() => setShares(Math.max(0, shares - 1))}
                 >
                   -
                 </button>
-                <div className="bg-[#ffffff] px-2 py-1 rounded border-1 text-skin w-[50px] mx-1">
-                  {shares}
-                </div>
+                <input
+                  type="number"
+                  id="amount"
+                  className="bg-[#ffffff] px-2 py-1 rounded border-1 text-skin w-[50px] mx-1"
+                  value={shares}
+                  onChange={(e) => {
+                    if (e.target.value == "") setShares(0);
+                    if (parseInt(e.target.value) >= 0)
+                      setShares(parseInt(e.target.value));
+                  }}
+                  min="0"
+                  required
+                />
                 <button
                   className="bg-[#ffffff] px-2 rounded-2xl border-1 w-[35px]"
                   onClick={() => setShares(shares + 1)}
@@ -178,9 +250,14 @@ export default function InvestmentPage() {
               </div>
             </div>
           </div>
-          Total: ${shares * assets[selectedTab][selectedInvestment].price}
+          Total: ${shares * investments[selectedTab][selectedInvestment].price}
           <div className="flex space-x-2 mt-2 justify-center text-2xl">
-            <button className="bg-green-500 text-[#ffffff] border-black py-2 rounded border-1 font-bold w-[70px]">
+            <button
+              className="bg-green-500 text-[#ffffff] border-black py-2 rounded border-1 font-bold w-[70px]"
+              onClick={() => {
+                if (shares > 0) setIsPop(true);
+              }}
+            >
               BUY
             </button>
             <button className="bg-red-600 text-[#ffffff] border-black py-2 rounded border-1 font-bold w-[70px]">
@@ -189,7 +266,7 @@ export default function InvestmentPage() {
           </div>
         </div>
         <div className="tabs flex flex-col w-[37px]">
-          {(Object.keys(assets) as Category[]).map((category) => (
+          {(Object.keys(investments) as Category[]).map((category) => (
             <button
               key={category}
               onClick={() => {
