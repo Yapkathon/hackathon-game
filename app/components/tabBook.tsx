d1ab5baa61e10174057d65ffd0dfc650cb3348ad
SorravichL <sorravichlakngoenchai@gmail.com>
2025-03-30 00:41:19 +0700

    expense assets

--- app/components/tabBook.tsx
+++ app/components/tabBook.tsx
@@ -3,7 +3,7 @@ import "./tabBook.css";
 
 export default function TabBook({ children }: { children: React.ReactNode }) {
   return (
-    <div className="tab-book w-full">
+    <div className="tab-book w-full h-full">
       <div className="flex flex-row">{children}</div>
     </div>
   );
